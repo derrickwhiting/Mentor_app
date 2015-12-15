41c4c36f30460230a2e9a4353aa2384c3e2f9186
Derrick Whiting <derrickwhiting@gmail.com>
2015-12-15 10:13:59 -0700

    adding functionality to create my own mentor app

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  has_many :lists
-  has_many :users, through: :lists
+  has_many :list_items
+  has_many :lists, through: :list_items
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :lists
-  has_many :items, through: :lists
 end
